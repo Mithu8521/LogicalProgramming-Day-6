c52fc00805805a4b5629800ba157f842000864a7
Mithu8521 <mithileshkr8521@gmail.com>
2021-08-24 15:16:30 +0530

    Print Reverse Number

---
+++ ReverseNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ReverseNumber {
+    public static void main (String[]args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int number = scan.nextInt();
+        ReverseNo(number);
+    }
+    static void ReverseNo(int number) {
+        int reverse = 0, remainder =0;
+        while(number>0){
+            remainder = number%10;
+            reverse= reverse*10+remainder;
+            number = number/10;
+        }
+        System.out.println("Reverse of the entered number is:"+reverse);
+    }
+} 
